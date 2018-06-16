4a06f195c11090aca1134ffc3a24ca5412bd8855
Unknown <luke123t@gmail.com>
2018-06-16 10:01:30 -0300

    Add DoNotDestroyOnLoad component

---
+++ Jalloo2018/Assets/Scripts/General/GameObjects/DoNotDestroyOnLoad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DogHouse.Jalloo.General
+{
+    /// <summary>
+    /// The Do Not Destroy on Load component can 
+    /// be attached to any game object that should
+    /// not be destroyed on load.
+    /// </summary>
+    public class DoNotDestroyOnLoad : MonoBehaviour
+    {
+        #region Main Methods
+        private void Awake() => DontDestroyOnLoad(gameObject);
+		#endregion
+	}
+}
