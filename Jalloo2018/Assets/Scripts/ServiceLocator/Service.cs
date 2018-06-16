4d9810842b423d3bc7087703015b73804754396f
Unknown <luke123t@gmail.com>
2018-06-16 09:18:40 -0300

    Add Service Locator core library

---
+++ Jalloo2018/Assets/Scripts/ServiceLocator/Service.cs
@@ -0,0 +1,11 @@
+﻿namespace DogHouse.Core.Services
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public interface IService 
+	{
+		void RegisterService ();
+	}
+}
